4440b2194aaf3bec06a782dc63a8911e2b3ea5d6
Yoav Ram <yoavram+github@gmail.com>
2012-10-26 09:22:17 +0200

    using the sumatra api

--- main.py
+++ main.py
@@ -4,25 +4,40 @@ from numpy import array
 import csv
 import time
 import sys
+from sumatra.projects import load_project
+from sumatra.parameters import build_parameters
 
-print "Strating"
-c = ConfigParser.ConfigParser()
-c.read(sys.argv[1])
+def main(parameters):
+    popsize = parameters["popsize"]
+    fpopsize = float(popsize)
+    ticks = parameters["ticks"]
 
-popsize = c.getint("default","popsize")
-fpopsize = float(popsize)
-ticks = c.getint("default","ticks")
+    x = [None]*ticks
+    x[0] = array([popsize/2, popsize/2])
+    t = 1
 
-x = [None]*ticks
-x[0] = array([popsize/2, popsize/2])
-t = 1
+    while t < ticks:
+        x[t] = multinomial(popsize, x[t-1]/fpopsize)
+        t += 1
+    fout = open(fout_name,"wb")
+    wr = csv.writer(fout)
+    wr.writerows(x)
+    fout.close()
 
-while t < ticks:
-    x[t] = multinomial(popsize, x[t-1]/fpopsize)
-    t += 1
+parameter_file = sys.argv[1]
+parameters = build_parameters(parameter_file)
 
-fout = open("Data/smt-test_"+str(time.time())+".csv","wb")
-wr = csv.writer(fout)
-wr.writerows(x)
-fout.close()
-print "Closing"
+project = load_project()
+record = project.new_record(parameters=parameters,
+                            main_file=__file__,
+                            reason="reason for running this simulation")
+parameters.update({"sumatra_label": record.label})
+start_time = time.time()
+
+main(parameters)
+
+record.duration = time.time() - start_time
+record.output_data = record.datastore.find_new_data(record.timestamp)
+project.add_record(record)
+
+project.save()
